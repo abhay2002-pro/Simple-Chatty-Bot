8230bf3aae93beddd4c10c2b71ff41e7f53c4269
unknown <abhayray2002@gmail.com>
2021-08-02 07:50:04 -0700

    First Commit Simple Chatty Bot

---
+++ Simple Chatty Bot/Topics/Types and variables/Assignments/src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Assign values to these four variables
+        int one=1,two=2,three=3,four=4;
+
+        System.out.println(String.format("%d %d %d %d", one, two, three, four));
+    }
+}
